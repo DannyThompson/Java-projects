c285dcc02bd337514ba609229659bb7d9dae46b3
DannyThompson <dannymthompson94@gmail.com>
2015-04-09 19:03:33 -0700

    Update Assignment5.java

--- Assignment5.java
+++ Assignment5.java
@@ -1,11 +1,8 @@
 package assignment5;
 
-/**
- *
- * @author Danny
- */
+ //@author Danny
+ 
 public class Assignment5 {
-
     /**
      * @param args the command line arguments
      */
@@ -13,42 +10,65 @@ public class Assignment5 {
        
     }
     public static class Node{
-        String token;
+        char token;
   
         public static Node leftChild;
         public static Node rightChild;
+        public static Node parent;
         
-        Node(String token){
+        Node(char token){
         this.token = token; 
         }
         
-        public String getToken(){
+        public char getToken(){
         return  token;
         }
     }
     
     public static class BinaryTree{
-        public Node root;
-        public void addNode(String token)
+        public static Node root;
+        public void addNode(char token)
         {
             Node newNode = new Node(token);
-            root = new Node(token);
+            //creating a root with an empty value
             if (root == null)
             {
                 root = newNode;
             }
             else{
-                
+                Node current = new Node(token);
+                current = root;
+                Node parent;
+            
+            //If statement to be followed if token is a '('
+            if (token == '('){
+               newNode = current.leftChild; 
             }
-    }
-    public void inOrder(Node focusNode){
-        if(focusNode != null)
-        {
-            inOrder(focusNode.leftChild);
-            System.out.println(focusNode);
-            inOrder(focusNode.rightChild);
+            //If statement to be followed if token is an operator
+            //**Code here is work in progress
+            if (token == '/'|| token== '*'|| token=='+'|| token == '-'){
+                current.token = token;
+            }
+            //If statement to be followed if token is ')"
+            if (token == ')'){
+                current = current.parent;
+            }
+            else{
+                //**Code here is work in progress.
+                //token will be last possibility: int 0-9
+                current.token = token;
+            }
+            root = new Node(token);
+            } 
         }
-        return;
-    }
+        public void inOrder(Node focusNode){
+            if(focusNode != null)
+            {
+                inOrder(focusNode.leftChild);
+                System.out.println(focusNode);
+                inOrder(focusNode.rightChild);
+            }
+            return;
+        }
     }
 }

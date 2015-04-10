383aea21df536daca063344f1c54a1f7dfa450b2
DannyThompson <dannymthompson94@gmail.com>
2015-04-09 17:56:41 -0700

    Create Assignment5.java

---
+++ Assignment5.java
@@ -0,0 +1,52 @@
+package assignment5;
+
+/**
+ *
+ * @author Danny
+ */
+public class Assignment5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    public static class Node{
+        String token;
+  
+        public static Node leftChild;
+        public static Node rightChild;
+        
+        Node(String token){
+        this.token = token; 
+        }
+        
+        public String getToken(){
+        return  token;
+        }
+    }
+    
+    public static class BinaryTree{
+        public Node root;
+        public void addNode(String token)
+        {
+            root = new Node(token);
+            
+            if (root == null)
+            {
+                root = newNode;
+            }
+            
+    }
+    public void inOrder(Node focusNode){
+        if(focusNode != null)
+        {
+            inOrder(focusNode.leftChild);
+            System.out.println(focusNode);
+            inOrder(focusNode.rightChild);
+        }
+        return;
+    }
+    }
+}
